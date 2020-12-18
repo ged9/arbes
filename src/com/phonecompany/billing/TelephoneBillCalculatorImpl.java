d14d83a051357ae9cc8a46ad9e76a840a31a3c92
ged9 <ged@seznam.cz>
2020-12-18 12:30:20 +0100

    Update TelephoneBillCalculatorImpl.java

--- src/com/phonecompany/billing/TelephoneBillCalculatorImpl.java
+++ src/com/phonecompany/billing/TelephoneBillCalculatorImpl.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
